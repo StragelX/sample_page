50fd1c230bfff9fe5093410ebd40e81c29164fa8
UkiePro <ukiepro@UkiePros-iMac.local>
2021-06-10 19:39:05 +0300

    f

--- js/scripts.js
+++ js/scripts.js
@@ -130,9 +130,10 @@ $( document ).ready(function() {
 
             responsive:{
                 0:{
+                    items: 1
                 },
                 700:{
-                    items: 3,
+                    items: 3
                 },
                 1000:{
                     items: 4
